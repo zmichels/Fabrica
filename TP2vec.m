245d0640664c7add2cc6277a6920edb12f896f8b
Zachary Michels <zachary.michels@gmail.com>
2020-09-16 22:26:58 -0500

    Update TP2vec.m
    This function has been updated to query the plotting preference in use (i.e. 'xAxisDirection' and 'zAxisDirection'), such that the result hopefully no longer depends on the convetion being used and will plot "geographically" correct regarless.

--- TP2vec.m
+++ TP2vec.m
@@ -1,2 +1,19 @@
 function [v] = TP2vec(trend,plunge)
+
+% is xvector preference set to east or west?
+xdir = getMTEXpref('xAxisDirection');
+if strcmp(xdir,'east') == 0
+    plunge = -plunge;
+end
+
+zdir = getMTEXpref('zAxisDirection');
+if strcmp(zdir,'outOfPlane') == 0
+    trend = -trend;
+    plunge = -plunge;
+end
+
 v = vector3d('polar',(90+plunge)*degree,(90-trend)*degree);
+
+if v.z>0
+    v = -v;
+end
