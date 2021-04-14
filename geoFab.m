55eedec91ca48c1ab5fc39cc47ab140b2eb05b25
Zachary Michels <zachary.michels@gmail.com>
2021-04-13 23:48:19 -0500

    Added geoFab
    geoFab is a more-generic version of SDT2or.m, for which you specify the downward lineation and foliation-pole directions as vector3d objects during input to the function.

---
+++ geoFab.m
@@ -0,0 +1,26 @@
+function [fabOr, fabRot, strikeV, poleV, linV] = geoFab(strike,dip,trend,linDir,poleDir)
+
+% This function takes three measurements of strike, trend, and plunge
+% in degrees and uses them to compute a rotational representation of a rock
+% fabric (i.e., a single quantity that combines foliation and lineation).
+
+% Dependencies: MTEX toolbox, TP2vec
+
+%% convert to vectors and compute poles to foliation
+% fabric dataset of strike, dip, trend, plunge to MTEX vectors
+strikeV = TP2vec(strike,zeros(size(strike)));
+trendV = TP2vec(trend,zeros(size(trend)));
+poleV = TP2vec(strike-90,90-dip);
+linV = cross(cross(trendV,zvector),poleV);
+
+poleV(poleV.z>0) = -poleV(poleV.z>0);
+linV(linV.z>0) = -linV(linV.z>0);
+
+
+%% Convert to orientation/rotation
+
+
+% Use fabric vectors to define principal axes of a fabric orientation
+fabRot = rotation('map',linDir, linV, poleDir, poleV);
+fabOr = orientation(fabRot,crystalSymmetry,'mmm');
+    
